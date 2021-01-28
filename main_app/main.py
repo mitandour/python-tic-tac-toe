3b87043a856b0f4ca65385142a2dc16e3160dc35
Mita Ndour <mitandour94@gmail.com>
2021-01-28 21:52:38 +0000

    Rectifying the result format

--- main_app/main.py
+++ main_app/main.py
@@ -196,7 +196,7 @@ def api_id():
         if board == "":
             board = "____o____".replace("_"," ")
             content = {'Board': board }
-            return "'"+ board.strip() + "'", status.HTTP_200_OK
+            return "'"+ content.Board + "'", status.HTTP_200_OK
         if len(board) < 9:
             while(len(board) < 9):
                 board = board + ' '
@@ -206,7 +206,7 @@ def api_id():
                 board = playGame(board)
                 board = board.replace("_"," ")
                 content = {'Board': board }
-                return "'"+ board.strip() + "'" , status.HTTP_200_OK
+                return "'"+ content.Board + "'" , status.HTTP_200_OK
             else:
                 content = {'Please try again': 'It\' not O\'s turn' }
                 return content, status.HTTP_400_BAD_REQUEST
